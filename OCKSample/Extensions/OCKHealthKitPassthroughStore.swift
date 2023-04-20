c3bd6d36d77a00a741f3754b0e77fa02554a37d3
Mjli235 <mjli235@uky.edu>
2023-04-20 14:51:59 -0400

    consent update

--- OCKSample/Extensions/OCKHealthKitPassthroughStore.swift
+++ OCKSample/Extensions/OCKHealthKitPassthroughStore.swift
@@ -71,7 +71,7 @@ extension OCKHealthKitPassthroughStore {
                    are trying to copy the method to this file, you are using the code incorrectly. Be
                    sure to understand the difference between a type method and instance method.
                    */
-           
+
            try await OCKStore.getCarePlanUUIDs() // This is a type method
         var steps = OCKHealthKitTask(
             id: TaskID.steps,
